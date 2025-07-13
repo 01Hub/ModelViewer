7ad881aa8cd70bf09094283618a5ae02f0bc7502
Sharjith Naramparambath <sharjith@gmail.com>
2025-07-13 22:39:04 +0530

    Removed hard coded title setting for Settings dialog

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -487,8 +487,7 @@ void MainWindow::on_actionSettings_triggered()
 	SettingsDialog* settingsDialog = new SettingsDialog(this);
 	settingsDialog->setAttribute(Qt::WA_DeleteOnClose);
 	settingsDialog->setMaxMSAASamples(ModelViewerApplication::supportedMSAASamples());
-	settingsDialog->setMaxAnisotropy(ModelViewerApplication::supportedAnisotropicFilteringLevel());
-	settingsDialog->setWindowTitle("Settings");
+	settingsDialog->setMaxAnisotropy(ModelViewerApplication::supportedAnisotropicFilteringLevel());	
 	settingsDialog->setModal(true);
 	settingsDialog->show();
 
